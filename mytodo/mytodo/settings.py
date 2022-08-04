d2c08a28722dbcf7057771ce859da278b2c64d0d
Donghyeon Lim <ldhyeon0891@naver.com>
2022-08-04 15:46:42 +0900

    Update settings.py

--- mytodo/mytodo/settings.py
+++ mytodo/mytodo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5iivvi8pv(#*8gw(f)h^cd&$xbhd!asjquwn!($!j7ijobi^^s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
